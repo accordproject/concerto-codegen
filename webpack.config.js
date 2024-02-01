204d0edac1708f1f96a96398dc3a38ecc96e911b
Stefan Blaginov <Stefan.Blaginov@docusign.com>
2024-02-01 15:03:25 +0000

    feat(deps): trying to polyfill v8
    Signed-off-by: Stefan Blaginov <stefan@blaginov.com>
    Signed-off-by: Stefan Blaginov <stefan.blaginov@docusign.com>

--- webpack.config.js
+++ webpack.config.js
@@ -70,7 +70,7 @@ module.exports = {
             'os': false,
             'util': false,
             'url': false,
-            'v8': require.resolve('v8/'),
+            'node:v8': require.resolve('node:v8/'),
         }
     }
 };
