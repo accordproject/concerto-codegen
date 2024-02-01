70360445522a8e760f13215b212c4754fb373574
Stefan Blaginov <Stefan.Blaginov@docusign.com>
2024-02-01 15:40:49 +0000

    refactor(models): improve error wording
    Signed-off-by: Stefan Blaginov <stefan@blaginov.com>
    Signed-off-by: Stefan Blaginov <stefan.blaginov@docusign.com>

--- test/common/benchmarkModelGenerator.js
+++ test/common/benchmarkModelGenerator.js
@@ -232,7 +232,7 @@ describe('benchmarkModelGenerator', function () {
             }).should.throw('growBy can be either set to "declarations" or "properties".');
         });
 
-        it('should throw an error if the requested model size exceeds the total_heap_size', function () {
+        it('should throw an error if the requested model size exceeds the available heap size and cannot be created', function () {
             const benchmarkModelGenerator = new BenchmarkModelGenerator();
             benchmarkModelGenerator.should.not.be.null;
 
@@ -242,7 +242,7 @@ describe('benchmarkModelGenerator', function () {
                     growBy: 'properties',
                     nDeclarations: 5,
                 });
-            }).should.throw('The requested model size is exceeding the total_heap_size and cannot be created.');
+            }).should.throw('The requested model size exceeds the available heap size and cannot be created.');
         });
     });
 });
