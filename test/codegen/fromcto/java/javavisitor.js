b692d4d56d8ecd0f27636aaee16cd72cb4e8c44f
Jaskeerat Singh Saluja <salujajaskeerat@Jaskeerats-MacBook-Pro.local>
2024-09-03 16:29:25 +0530

    feat(import-alias): concerto-core version updated and minor changes
    Signed-off-by: Jaskeerat Singh Saluja <salujajaskeerat@Jaskeerats-MacBook-Pro.local>

--- test/codegen/fromcto/java/javavisitor.js
+++ test/codegen/fromcto/java/javavisitor.js
@@ -583,7 +583,7 @@ describe('JavaVisitor', function () {
             param.fileWriter.writeLine.getCall(2).args.should.deep.equal([1, '}']);
         });
 
-        it('should write a line getting a field of type aliased import', ()=> {
+        it('should write a line getting a field of type aliased import', () => {
             // Document aliased as Doc
             let mockField = sinon.createStubInstance(Field);
             let mockModelFile = sinon.createStubInstance(ModelFile);
@@ -596,7 +596,7 @@ describe('JavaVisitor', function () {
             mockModelFile.isImportedType.returns(true);
             mockModelFile.getImportedType.returns('Document');
 
-            javaVisit.visitField(mockField , Object.assign({},param,{mode:'getter'}));
+            javaVisit.visitField(mockField, Object.assign({}, param, { mode: 'getter' }));
             param.fileWriter.writeLine.callCount.should.deep.equal(3);
             param.fileWriter.writeLine.getCall(0).args.should.deep.equal([1, 'public Document getBob() {']);
             param.fileWriter.writeLine.getCall(1).args.should.deep.equal([2, 'return this.Bob;']);
